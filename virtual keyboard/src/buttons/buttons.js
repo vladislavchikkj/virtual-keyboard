1be2209ebc74ff54cbec75e8e34d02fb2ab8f3e3
Vlad <vlad.kosevich@mail.ru>
2023-04-21 00:41:26 +0300

    feat: refactor code

--- virtual keyboard/src/buttons/buttons.js
+++ virtual keyboard/src/buttons/buttons.js
@@ -3,64 +3,73 @@ export const russianKeyboardButtons = [
   ["ё1234567890-=", { key: "Backspace", type: "control" }],
 
   // Ряд 2
-  [
-    { key: "Tab", type: "control" },
-    "йцукенгшщзхъ\\",
-    { key: "CapsLock", type: "control" },
-  ],
+  [{ key: "Tab", type: "control" }, "йцукенгшщзхъ\\"],
 
   // Ряд 3
-  ["фывапролджэ", { key: "Enter", type: "control" }],
+  [
+    { key: "CapsLock", type: "control" },
+
+    "фывапролджэ",
+    { key: "Enter", type: "control" },
+  ],
 
   // Ряд 4
   [
     { key: "Shift", type: "control" },
     "ячсмитьбю.",
-    { key: "ArrowUp", type: "arrow" },
+    { key: "▲", type: "arrow" },
     { key: "Shift", type: "control" },
   ],
 
   // Ряд 5
   [
     { key: "Control", type: "control" },
+    { key: "Win", type: "control" },
     { key: "Alt", type: "control" },
     { key: " ", type: "space" },
     { key: "Alt", type: "control" },
-    { key: "ArrowLeft", type: "arrow" },
-    { key: "ArrowDown", type: "arrow" },
-    { key: "ArrowRight", type: "arrow" },
     { key: "Control", type: "control" },
+    { key: "◄", type: "arrow" },
+    { key: "▼", type: "arrow" },
+    { key: "►", type: "arrow" },
   ],
 ];
 
 export const englishKeyboardButtons = [
-  // Ряд 1
-  "`~1!2@3#4$5%6^7&8*9(0)-_+=",
-  { key: "Backspace", type: "control" },
-
-  // Ряд 2
-  { key: "Tab", type: "control" },
-  "qwertyuiop[]\\",
-  { key: "CapsLock", type: "control" },
-
-  // Ряд 3
-  { key: "a", shiftKey: "A" },
-  "sdfghjkl;'",
-  { key: "Enter", type: "control" },
-
-  // Ряд 4
-  { key: "Shift", type: "control" },
-  "zxcvbnm,./",
-  { key: "ArrowUp", type: "arrow" },
-  { key: "Shift", type: "control" },
-
-  // Ряд 5
-  { key: "Control", type: "control" },
-  { key: "Alt", type: "control" },
-  { key: " ", type: "space" },
-  { key: "Alt", type: "control" },
-  { key: "ArrowLeft", type: "arrow" },
-  { key: "ArrowDown", type: "arrow" },
-  { key: "ArrowRight", type: "arrow" },
-  { key: "Control", type: "control" },
+  [
+    // Ряд 1
+    "`1234567890-=",
+    { key: "Backspace", type: "control" },
+  ],
+  [
+    // Ряд 2
+    { key: "Tab", type: "control" },
+    "qwertyuiop[]\\",
+  ],
+  [
+    // Ряд 3
+    { key: "CapsLock", type: "control" },
+    { key: "a", shiftKey: "A" },
+    "sdfghjkl;'",
+    { key: "Enter", type: "control" },
+  ],
+  [
+    // Ряд 4
+    { key: "Shift", type: "control" },
+    "zxcvbnm,./",
+    { key: "▲", type: "arrowUp" },
+    { key: "Shift", type: "control" },
+  ],
+  [
+    // Ряд 5
+    { key: "Control", type: "control" },
+    { key: "Win", type: "control" },
+    { key: "Alt", type: "control" },
+    { key: " ", type: "space" },
+    { key: "Alt", type: "control" },
+    { key: "Control", type: "control" },
+    { key: "◄", type: "arrowLeft" },
+    { key: "▼", type: "arrowDown" },
+    { key: "►", type: "arrowRight" },
+  ],
 ];
--- virtual keyboard/src/keyboard/keyboard.js
+++ virtual keyboard/src/keyboard/keyboard.js
@@ -2,60 +2,86 @@ import "./_keyboard.scss";
 import * as buttons from "../buttons/buttons";
 
 const renderKeyboard = () => {
+  // Создаем элементы и добавляем классы
   const textField = document.createElement("textarea");
   textField.classList.add("text-field");
 
-  const keyboardButtons = buttons.russianKeyboardButtons;
+  const changeLang = document.createElement("button");
+  changeLang.classList.add("change-lang-btn");
+  changeLang.textContent = "ru";
 
-  // функция для обработки нажатия на кнопки клавиатуры
-  function handleKeyboardButtonClick(event) {
-    const clickedButton = event.target;
-    if (clickedButton.tagName === "BUTTON") {
-      const buttonValue = clickedButton.textContent;
-      if (buttonValue === "Backspace") {
-        textField.value = textField.value.slice(0, -1);
-      } else if (buttonValue === "Enter") {
-        textField.value += "\n";
-      } else if (buttonValue === "Space") {
-        textField.value += " ";
-      } else {
-        textField.value += buttonValue;
-      }
-    }
-  }
+  let keyboardButtons = buttons.russianKeyboardButtons;
 
   const keyboard = document.createElement("div");
   keyboard.classList.add("keyboard");
 
-  keyboardButtons.forEach((row) => {
-    const rowEl = document.createElement("div");
-    rowEl.classList.add("keyboard__row");
+  // Функция обработчика нажатия на кнопки клавиатуры
+  function handleKeyboardButtonClick(event) {
+    if (event.target.tagName !== "BUTTON") return;
 
-    row.forEach((btn) => {
-      const btnEl = document.createElement("button");
-      btnEl.classList.add("keyboard__button", `keyboard__button--${btn.type}`);
-      if (typeof btn === "string") {
-        btn.split("").forEach((char) => {
-          const charBtnEl = document.createElement("button");
-          charBtnEl.classList.add("keyboard__button");
-          charBtnEl.textContent = char;
-          charBtnEl.addEventListener("click", handleKeyboardButtonClick);
-          rowEl.appendChild(charBtnEl);
-        });
-      } else {
-        btnEl.textContent = btn.key;
-        btnEl.addEventListener("click", handleKeyboardButtonClick);
-        rowEl.appendChild(btnEl);
-      }
+    const { textContent } = event.target;
+    if (textContent === "Backspace") {
+      textField.value = textField.value.slice(0, -1);
+    } else if (textContent === "Enter") {
+      textField.value += "\n";
+    } else if (textContent === "Space") {
+      textField.value += " ";
+    } else {
+      textField.value += textContent;
+    }
+  }
+
+  // Функция рендера кнопок клавиатуры
+  function renderBtn() {
+    keyboard.innerHTML = "";
+
+    keyboardButtons.forEach((row) => {
+      const rowEl = document.createElement("div");
+      rowEl.classList.add("keyboard__row");
+
+      row.forEach((btn) => {
+        const btnEl = document.createElement("button");
+        btnEl.classList.add(
+          "keyboard__button",
+          `keyboard__button--${btn.type}`
+        );
+
+        if (typeof btn === "string") {
+          btn.split("").forEach((char) => {
+            const charBtnEl = document.createElement("button");
+            charBtnEl.classList.add("keyboard__button");
+            charBtnEl.textContent = char;
+            charBtnEl.addEventListener("click", handleKeyboardButtonClick);
+            rowEl.appendChild(charBtnEl);
+          });
+        } else {
+          btnEl.textContent = btn.key;
+          btnEl.addEventListener("click", handleKeyboardButtonClick);
+          rowEl.appendChild(btnEl);
+        }
+      });
+
+      keyboard.appendChild(rowEl);
     });
+  }
 
-    keyboard.appendChild(rowEl);
+  renderBtn();
+
+  // Обработчик нажатия на кнопку смены языка
+  changeLang.addEventListener("click", () => {
+    keyboardButtons =
+      keyboardButtons === buttons.russianKeyboardButtons
+        ? buttons.englishKeyboardButtons
+        : buttons.russianKeyboardButtons;
+    changeLang.textContent =
+      keyboardButtons === buttons.russianKeyboardButtons ? "ru" : "eng";
+    renderBtn();
   });
-  // функция для обработки нажатия на кнопки клавиатуры
+
+  // Обработчики нажатия на клавиатуре
   document.addEventListener("keydown", (event) => {
-    const key = event.key;
-    const keyboardButton = document.querySelector(
-      `.keyboard__button[data-key="${key}"]`
+    const keyboardButton = keyboard.querySelector(
+      `.keyboard__button[data-key="${event.key}"]`
     );
     if (keyboardButton) {
       keyboardButton.classList.add("keyboard__button--pressed");
@@ -64,17 +90,16 @@ const renderKeyboard = () => {
   });
 
   document.addEventListener("keyup", (event) => {
-    const key = event.key;
-    const keyboardButton = document.querySelector(
-      `.keyboard__button[data-key="${key}"]`
+    const keyboardButton = keyboard.querySelector(
+      `.keyboard__button[data-key="${event.key}"]`
     );
     if (keyboardButton) {
       keyboardButton.classList.remove("keyboard__button--pressed");
     }
   });
 
-  document.body.appendChild(textField);
-  document.body.appendChild(keyboard);
+  // Добавляем элементы на страницу
+  document.body.append(textField, keyboard, changeLang);
 };
 
 export default renderKeyboard;
